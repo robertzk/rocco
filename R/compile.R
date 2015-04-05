fa0b825d8b2769f095cf02a66c345cadc0ae9ff5
RobertZK <technoguyrob@gmail.com>
2015-04-04 22:59:24 -0500

    initial full rocco compilation

--- R/compile.R
+++ R/compile.R
@@ -14,18 +14,74 @@ rocco_data <- function(pkg_dir) {
   list(
     package_description = gsub("[[:space:]]+", " ", package_description(pkg_dir)),
     package_title = package_title(pkg_dir),
-    sections = list(
-      list(
-        commentary = markdown::markdownToHTML(fragment.only = TRUE, text = "# How's it going?\n\nI am markdown!"),
-        code       = "library('test')\nfoo <- function(a, b) {\n  a + b\n}\ncat('Hi!')"
-      ),
-
-      list(
-        commentary = markdown::markdownToHTML(fragment.only = TRUE, text = "# How's it going?\n\nI am more markdown!"),
-        code       = "library('test')\nfoo <- function(a, b, c) {\n  a + b - c\n}\ncat('Hello')"
-      )
-    )
+    sections = package_sections(pkg_dir)
   )
 }
 
+package_sections <- function(pkg_dir) {
+  do.call(c, lapply(
+    list.files(file.path(pkg_dir, "R"), full.names = TRUE), 
+    file_section
+  ))
+}
+
+file_section <- function(file) {
+  lines <- readLines(file)
+
+  c(
+    list(file_header(file)),
+    rocco_sections(lines)
+  )
+}
+
+file_header <- function(file) {
+  list(commentary = markdown_to_html(paste0("# ", basename(file))), code = "")
+}
+
+rocco_sections <- function(lines) {
+  rocco_lines <- rocco_lines(lines)
+  unname(tapply(
+    lines,
+    cumsum(c(0, diff(rocco_lines)) == 1),
+    FUN = rocco_section
+  ))
+}
+
+rocco_regex <- "^[[:space:]]+## "
+
+rocco_lines <- function(lines) {
+  grepl(rocco_regex, lines)
+}
+
+rocco_section <- function(lines) {
+  section <- split(lines, rocco_lines(lines))
+  list(
+    commentary = rocco_commentary(section$`TRUE` %||% character(0)),
+    code       = paste(section$`FALSE` %||% character(0), collapse = "\n")
+  )
+}
+
+rocco_commentary <- function(lines) {
+  markdown_to_html(paste(gsub(rocco_regex, "", lines), collapse = "\n"))
+}
+
+markdown_to_html <- function(text) {
+  if (length(text) > 0 && nzchar(text)) {
+    markdown::markdownToHTML(text = text, fragment.only = TRUE)
+  } else ""
+}
+
+{
+  list(
+    list(
+      commentary = markdown::markdownToHTML(fragment.only = TRUE, text = "# How's it going?\n\nI am markdown!"),
+      code       = "library('test')\nfoo <- function(a, b) {\n  a + b\n}\ncat('Hi!')"
+    ),
+
+    list(
+      commentary = markdown::markdownToHTML(fragment.only = TRUE, text = "# How's it going?\n\nI am more markdown!"),
+      code       = "library('test')\nfoo <- function(a, b, c) {\n  a + b - c\n}\ncat('Hello')"
+    )
+  )
+}
 
